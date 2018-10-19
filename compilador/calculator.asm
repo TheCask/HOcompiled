3b1c13bc419b275a5db2c548a79f102baf81c1db
Eugenio Cálcena <cask@MBPr.local>
2018-10-19 14:57:20 -0300

    subo archivos intermedios compliacion y modifico .gitignore xq los estaba ignorando

---
+++ compilador/calculator.asm
@@ -0,0 +1,60 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 10, 14
+	.intel_syntax noprefix
+	.globl	_main                   ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset rbp, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register rbp
+	sub	rsp, 32
+	mov	eax, 31
+	mov	ecx, 11
+	mov	dword ptr [rbp - 4], 0
+	mov	dword ptr [rbp - 8], edi
+	mov	qword ptr [rbp - 16], rsi
+	mov	edi, eax
+	mov	esi, ecx
+	call	_add_numbers
+	lea	rdi, [rip + L_.str]
+	mov	dword ptr [rbp - 20], eax
+	mov	esi, dword ptr [rbp - 20]
+	mov	al, 0
+	call	_printf
+	xor	ecx, ecx
+	mov	dword ptr [rbp - 24], eax ## 4-byte Spill
+	mov	eax, ecx
+	add	rsp, 32
+	pop	rbp
+	ret
+	.cfi_endproc
+                                        ## -- End function
+	.globl	_add_numbers            ## -- Begin function add_numbers
+	.p2align	4, 0x90
+_add_numbers:                           ## @add_numbers
+	.cfi_startproc
+## %bb.0:
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset rbp, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register rbp
+	mov	dword ptr [rbp - 4], edi
+	mov	dword ptr [rbp - 8], esi
+	mov	esi, dword ptr [rbp - 4]
+	add	esi, dword ptr [rbp - 8]
+	mov	eax, esi
+	pop	rbp
+	ret
+	.cfi_endproc
+                                        ## -- End function
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"I know how to add! 31 + 11 is %d\n"
+
+
+.subsections_via_symbols
